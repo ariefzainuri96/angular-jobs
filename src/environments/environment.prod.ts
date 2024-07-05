0286f8f37a19a0f7ff4dd2ac3a31979c729874b8
Arief Zainuri <flaminglassoo1996@gmail.com>
2024-07-05 18:32:38 +0700

    Updates

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://backend-frontend-jobs.vercel.app/angular',
-  baseApiUrl: 'https://backend-frontend-jobs.vercel.app',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://backend-frontend-jobs.vercel.app/angular',
-  baseApiUrl: 'https://backend-frontend-jobs.vercel.app',
-};
